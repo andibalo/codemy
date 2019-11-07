1001e038f0fc3d43fcf822b904bede7daf3db2bb
andi balo <andibalo213@gmail.com>
2019-11-07 14:19:52 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+//NOTE: WHY WINDOW NOT DOCUEMENT
+
+$(window).on("load", () => {
+  $("#loaderImage").fadeOut();
+
+  //differnece between delay and giving seconds to fadeout is on fadeout is the length of animation
+  //but with delay stops the program for a set time
+  $("#preloader")
+    .delay(350)
+    .fadeOut();
+});
+
+const showHideNav = () => {
+  if ($(window).scrollTop() > 50) {
+    $(".navbar").addClass("navBlue");
+  } else {
+    $(".navbar").removeClass("navBlue");
+  }
+};
+
+$(window).on({
+  scroll: () => {
+    showHideNav();
+  },
+  load: () => {
+    showHideNav();
+  }
+});
+
+//TYPEWRITER EFFECT
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
